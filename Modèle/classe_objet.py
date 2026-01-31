046a642b897375def9efed1a7a85ef76fb2d99ad
atunc01 <ahmet.tunc@insa-strasbourg.fr>
2026-01-31 12:49:50 +0100

    début d'ecriture des classes coté python

--- Modèle/classe_objet.py
+++ Modèle/classe_objet.py
@@ -1,4 +1,5 @@
 from datetime import date
+from Gestion_db import OptionsPossibles
 
 class Client:
 
@@ -18,4 +19,19 @@ class Reservation:
         self.start_date=start_date
         self.end_date=end_date
 
+
+class Chambre:
+
+    def __init__(self, room_id:int, max_people:int, price:int, room_size:int):
+        self.room_id=room_id
+        self.max_people=max_people
+        self.price=price
+        self.room_size=room_size
+
+
+class Option:
+
+    def __init__(self, reservation_id:int, option_id:OptionPossibles) 
+        self.reservation_id=reservation_id
+        self.option_id=option_id
 
